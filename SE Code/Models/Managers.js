c31484d7c2db6dd637d08135b3f0756e60641fbe
ziadDarwish <41762572+ziadDarwish@users.noreply.github.com>
2019-03-02 22:50:40 +0200

    Managers For SPC Forms

---
+++ SE Code/Models/Managers.js
@@ -0,0 +1,21 @@
+class Managers{
+
+
+    constructor(id,name,type,gender,nationality,idType,ssn,birthdate,address,rank){
+
+        this.id=id;
+        this.name=name;
+        this.type=type;
+        this.gender=gender;
+        this.nationality=nationality;
+        this.idType=idType;
+        this.ssn=ssn;
+        this.birthdate=birthdate;
+        this.address=address;
+        this.rank=rank;
+
+    }
+
+}
+
+module.exports= Managers;

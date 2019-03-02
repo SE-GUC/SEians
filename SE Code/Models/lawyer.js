b21eb10afbddef49274965487686d792f37c8b29
abdulrahman1100 <47214336+abdulrahman1100@users.noreply.github.com>
2019-03-02 21:52:33 +0200

    Add files via upload

---
+++ SE Code/Models/lawyer.js
@@ -0,0 +1,11 @@
+class lawyer {
+    constructor(id,username,fullname,email, password) {
+        this.id = id;
+        this.username = username;
+        this.fullname = fullname;
+        this.email = email;
+        this. password =  password;
+    };
+}
+
+module.exports = lawyer

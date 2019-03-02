0fa07cca0750e6707e30ad68e1aa730773d3bec3
MohammadAshraf10 <47257783+MohammadAshraf10@users.noreply.github.com>
2019-03-02 22:35:17 +0200

    Update index.js

--- SE Code/index.js
+++ SE Code/index.js
@@ -4,11 +4,10 @@ const express = require('express')
 
 // Require Router Handlers
 const Admins = require('./Routes/api/Admins')
-
 const reviewers = require('./Routes/api/reviewers')
-
-
+const lawyers = require('./Routes/api/lawyers')
 const investors = require('./Routes/api/investors')
+const Companies= require('./Routes/api/Companies')
 
 
 
@@ -25,7 +24,7 @@ app.use(express.json())
       
     
 
-      res.send(' <a href="/api/Admins">Admins</a> <br> <a href="/api/reviewers">Reviewers</a> <br> <a href="/api/investors">investors</a>');
+      res.send(' <a href="/api/Admins">Admins</a> <br> <a href="/api/reviewers">Reviewers</a> <br> <a href="/api/investors">investors</a> <br><a href="/api/lawyers">lawyers</a><br><a href="/api/Companies">Companies</a> ');
 
   })
   
@@ -39,7 +38,9 @@ app.use('/api/reviewers', reviewers)
 
 app.use('/api/investors', investors)
 
+app.use('/api/lawyers',lawyers)
 
+app.use('/api/Companies',Companies)
 // Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});

eecd453245d7aa3b0079d30f6e074f0654d3c2ee
ziadDarwish <ziaddarwish97@yahoo.com>
2019-04-04 19:39:47 +0200

    Revert "Merge pull request #43 from SE-GUC/essamwnabil"
    This reverts commit b33b58545a01aeedff326333e9fb5ea3ef21c926, reversing
    changes made to 0bf4c063202e10932861a0a35bf6e9f9a37e10a5.

--- test.js
+++ test.js
@@ -1,24 +1,98 @@
 
-const funcs = require('./enfunctions');
+const funcs = require('./fnInvestor');
 
-test('requests should come as an array', async () => {
-  expect.assertions(1);
-  const task = await funcs.gettasks();
-  expect(Array.isArray(lawyers.data.cases)).toBe(true);
-});
 
-test('view un reviewed', async () => {
-  const unreviewed = await funcs.getunreviewed();
-  expect(Array.isArray(reviewer.data.unreviewed)).toBe(true);
-})
+  test('POST should create a new investor', async (done) => {
+    const investors = await funcs.createinvestor();
+    investorss = investors.data.data;
+    expect(investorss.userName).toBe('saleh');
+    done()
+  })
 
-test('get all cases', async () => {
-  const cases = await funcs.getallcases();
-  expect(Array.isArray(Admin.data.cases)).toBe(true);
-});
+  test(`investor's name should be saleh`, async (done) => {
+    let investors = await funcs.get()
+    investorss = investors.data.data;
+    //console.log(investorss.some(investor => investor.userName === 'saleh'))
+     expect(investorss.some(investor => investor.userName === 'saleh')).toBe(true)
+    done()
+  } );
 
-test('request status to be boolean', async () => {
-  const accepted = await funcs.trackrequeststate();
-  expect(accepted).toBe('true');
-});
+  test('GET specific investor', async (done) => {
+    const investors = await funcs.getinvestor();
+    investorss = investors.data.data;
+    expect(investorss.userName).toBe('saleh');
+    done()
+  });
 
+  test('UPDATE specific investor', async (done) => {
+    const investors = await funcs.updateinvestor();
+    investorss = investors.data.data;
+    expect(investorss.name).toBe('mohamed');
+    done()
+  });
+  
+  test('DELETE specific investor', async (done) => {
+    let investors = await funcs.deleteinvestor()
+    investorss = investors.data.data;
+    //console.log(investorss.some(investor => investor.userName === 'saleh'))
+    expect(investorss.some(investor => investor.userName === 'saleh')).toBe(false)
+    done()
+
+  });
+
+global.Promise=jest.requireActual('promise');
+const funcs = require('./spcfn');
+
+  test(`Created a company with a name of 'hamada'`, async (done) => {
+    //expect.assertions(0)
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+
+    let Spcs = await funcs.createSpc()
+    Spcss = Spcs.data.data;
+     expect(Spcss.companyName).toBe('hamada2');
+    done()
+  } );
+
+
+
+  test(` get All Spc`, async (done) => {
+  
+    let Spcs = await funcs.getSpcAll()
+    Spcss = Spcs.data.data;
+    console.log(Spcss.some(Spc => Spc.companyName === 'hamada2'))
+     expect(Spcss.some(Spc => Spc.companyName === 'hamada2')).toBe(true)
+    done()
+  } );
+
+  test('UPDATE specific Spc', async (done) => {
+    const Spcs = await funcs.updateSpc();
+    Spcss = Spcs.data.data;
+    expect(Spcss.companyName).toBe('mesh hamada');
+    done()
+  });
+
+
+  test('GET specific Spc', async (done) => {
+    const Spcs = await funcs.getSpc();
+    Spcss = Spcs.data.data;
+    expect(Spcss.companyName).toBe('mesh hamada');
+    done()
+  });
+
+ 
+
+
+test('DELETE specific Spc', async (done) => {
+    let Spcs = await funcs.deleteSpc()
+    Spcss = Spcs.data.data;
+    //console.log(Spcss.some(Spc => Spc.companyName === 'hamada'))
+     expect(Spcss.some(Spc => Spc.companyName === 'mesh hamada')).toBe(false)
+    done()
+  }); 
+
+  afterAll(async done => {
+    // Closing the DB connection allows Jest to exit successfully.
+    mongoose.disconnect()
+    done();
+  });
+  

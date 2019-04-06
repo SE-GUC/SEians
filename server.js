98053127de9f1feb2e36e09f4759f30f70089303
karimfarahat <karimhosny99@gmail.com>
2019-04-06 23:55:05 +0200

    zabat el dev

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const Spcs = require('./Routes/api/Spcs')
 const SscFormController= require('./Routes/api/SscFormController')
 
 
-const bodyParser = require('body-parser');//needs a midleware 
-const passport = require('passport');//needs a midleware 
+const bodyParser = require('body-parser');//needs a midleware
+const passport = require('passport');//needs a midleware
 
 
 const app = express()
@@ -43,7 +43,7 @@ const {
   //passport midleware
 app.use(passport.initialize());
 
-//passport config jwt autherization strategy 
+//passport config jwt autherization strategy
 
 require('./config/passport')(passport)
 
@@ -51,7 +51,7 @@ require('./config/passport')(passport)
      mongoose.connect(`${MONGO_DNS_SRV}${MONGO_AUTH}${MONGO_CLUSTER}${MONGO_DB_NAME}${MONGO_OPTIONS}`, {
        useNewUrlParser: true
      })
-  
+
     app.use(express.json())
 
 
@@ -67,7 +67,7 @@ app.get('/', (req, res) => {
       res.send(' <a href="/api/Spcs">Spcs</a> <br><a href="/api/Admins">Admins</a> <br> <a href="/api/reviewers">Reviewers</a> <br> <a href="/api/investors">investors</a> <br><a href="/api/lawyers">lawyers</a><br><a href="/api/Companies">Companies</a> ');
 
   })
-  
+
 
 
 
@@ -88,5 +88,5 @@ app.use('/api/reviewer', reviewers)
 
 app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
 
-const port = 3000
+const port = 5000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))

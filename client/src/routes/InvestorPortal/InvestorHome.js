25df7fccf7f33268075253cf614739fbf2719748
rawdaGastan <47260239+rawdaGastan@users.noreply.github.com>
2019-04-23 02:08:12 +0200

    Update InvestorHome.js
    zabat el home bta3t investor w 7atet bta3tak mt5afsh get company

--- client/src/routes/InvestorPortal/InvestorHome.js
+++ client/src/routes/InvestorPortal/InvestorHome.js
@@ -1,64 +1,63 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from '../../components/Layout/Header';
-import Footer from '../../components/Layout/Footer';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../../components/Layout/Header";
+import Footer from "../../components/Layout/Footer";
+import { withRouter } from "react-router";
 import Menu from '../../components/Layout/Menu';
 import logo from '../../components/Layout/img/logo.png';
 
+class InvestorHome extends Component { 
 
-export class InvestorHome extends Component{
-render(){
-  let links = [
-    { label: 'Home', link: '#home', active: true },
-    { label: 'About', link: '#about' },
-    { label: 'View Company', link: '/GetCompany1' },
-    { label: 'Log Out', link: '#log-out' }
-
-  ];
-
-return(
-<div>
-  <Router>
-<div className="NavigationBar">
-      <div className="container">
-      <Menu links={links} logo={logo} />
-        <body>
-      <Header name="Home" /></body>
-        <Route exact path="/" render={props => (
-          <React.Fragment>
-           <li><a href="default.asp">Home</a></li>
-           <li><a href="news.asp">News</a></li>
-           <li><a href="contact.asp">Contact</a></li>
-           <li><a href="about.asp">About</a></li>
-           </React.Fragment>
-         )} />
-         <Route path="/about" />
-       </div>
-     </div>
-    <footer className="Footer">
-    <Footer/>
-    </footer>
-      </Router>
-
-      <div style= {{marginTop: '100px', marginLeft: '500px'}}>
-      <li className="nav-item">
-      <a href="/Register">Register as investor</a>
-      </li>
-      <li className="nav-item">
-      <a href="/Login">login as investor</a>
-      </li>
-      <li className="nav-item">
-      <a href="/SearchInvestor">searchINInvestor</a>
-      </li>
-      <li className="nav-item">
-      <a href="/UpdateInvestor">UpdateInvestor</a>
-      </li>
-      <li className="nav-item">
-      <a href="/DeleteInvestor">deleteInvestor</a>
-      </li>
-    </div>
-</div>
-);
+  render() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    let name = user.name
+    let links = [
+      { label: name , link: '/Profile', active: true },
+      { label: 'search' , link: '/SearchInvestor', active: true },
+      { label: 'edit' , link: '/UpdateInvestor', active: true },
+      { label: 'delete' , link: '/deleteInvestor', active: true },
+      { label: 'show Companies' , link: '/CompanyViewer', active: true },
+      { label: 'Logout', link: '/LogoutInv', active: true },
+      { label: 'Get My Forms', link: '/getMyForms', active: true },
+      { label: 'View Company', link: '/GetCompany' },
+    ];
+    return (
+      <div>
+        <Router>
+          <div className="NavigationBar">
+            <div className="container">
+            <Menu links={links} logo={logo} />
+              <Header name="Home" />
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <React.Fragment>
+                    <li>
+                      <a href="default.asp">Home</a>
+                    </li>
+                    <li>
+                      <a href="news.asp">News</a>
+                    </li>
+                    <li>
+                      <a href="contact.asp">Contact</a>
+                    </li>
+                    <li>
+                      <a href="about.asp">About</a>
+                    </li>
+                  </React.Fragment>
+                )}
+              />
+              <Route path="/about" />
+            </div>
+          </div>
+          <footer className="Footer">
+            <Footer />
+          </footer>
+        </Router>
+      </div>
+    );
+  }
 }
-}
-export default InvestorHome
+export default withRouter(InvestorHome);
